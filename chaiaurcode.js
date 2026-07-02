bd0a51f314208656b0b6e9b790c9142625c82f6b
anujkumar-11 <24bec021@nith.ac.in>
2026-07-02 14:08:09 +0530

    js

--- chaiaurcode.js
+++ chaiaurcode.js
@@ -1,1 +1,9 @@
 
+const clock = document.getElementById('clock');
+
+
+setInterval(function()){
+   let date = new Date();
+  clock.innerHTML = date.toLocalTimeString();
+},1000);
+// 1000 => means 1 second
